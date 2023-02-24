ad5aa08e7025c5e83885906e73adf237a943ef2e
Abraham <Abraham12orca@gmail.com>
2023-02-24 18:27:43 +0100

    Firebase Auth
    Added some necessary files in order to make the Firebase Auth service work

---
+++ lib/services/auth.dart
@@ -0,0 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  /* Generating the variable that will communicate with
+  * the Auth services */
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //TODO: sign in anonymously method
+  Future signInAnon() async {
+    try {} catch (e) {}
+  }
+
+//TODO: sign in with email and password method
+
+//TODO: register with email and password method
+
+//TODO: sign out method
+}
